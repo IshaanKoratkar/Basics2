9e1d7ace8634f43f8eb36e3ca9d93756a364de70
Ashwin Kalyan <64381851+Ashwin-Kalyan@users.noreply.github.com>
2020-04-28 20:14:44 -0500

    Update Notes.py

--- Notes.py
+++ Notes.py
@@ -6,7 +6,7 @@ import os
 
 window = Tk()
 window.geometry("1000x450") 
-window.title("Notes")
+window.title("Notes")  
 
 scrollbar = Scrollbar(window)
 scrollbar.pack(side = RIGHT, fill = Y)
@@ -16,6 +16,28 @@ typingarea.pack()
 
 icon = PhotoImage(file = "Notes_Icon.png") 
 window.iconphoto(False, icon) 
+
+def new(): 
+
+    if messagebox.askyesno("Save", "Would you like to save the currently open file?"):
+
+        file_extension = [("Text Document (*.txt)", "*.txt"), 
+                         ("Rich Text Format (*.rtf)", "*.rtf"), 
+                         ("Microsoft Word Open XML Document (*.docx)", "*.docx"), 
+                         ("Microsoft Word Document (*.doc)", "*.doc"), 
+                         ("Portable Document Format (*.pdf)", "*.pdf")]
+        text_file = asksaveasfile(mode = "w", filetypes = file_extension, defaultextension = file_extension, title = "Save")   
+        data = typingarea.get(0.0, END) 
+        text_file.write(data) 
+        text_file.close()
+        
+        file = None
+        typingarea.delete(1.0, END)  
+
+    else:
+
+        file = None
+        typingarea.delete(1.0, END)     
 
 def open():
 
@@ -44,16 +66,38 @@ def exit():
 
         window.destroy()     
 
+
+def copy():
+
+    typingarea.event_generate("<<Copy>>") 
+
+def paste():
+
+    typingarea.event_generate("<<Paste>>") 
+
+def cut(): 
+
+    typingarea.event_generate("<<Cut>>")     
+
+def view_help():
+
+    showinfo("View Help", "Wdym this is app is soo easy.")  
+
+def about_notes():
+
+    showinfo("About Notes", "Notes 0.1 Beta\nA simple text editor written in Python similar to Window's bulit-in Notepad.\nBuilt by Ashwin Kalyan and Ishaan Koratkar.")
+
 menubar = Menu(window)
 
 filemenu = Menu(menubar, tearoff = 0)
 menubar.add_cascade(label = "File", menu = filemenu) 
-filemenu.add_command(label = "New                             ", activebackground = "light sky blue") 
+filemenu.add_command(label = "New                             ", activebackground = "light sky blue", command = new) 
 filemenu.add_command(label = "New Window                      ", activebackground = "light sky blue")  
+filemenu.add_separator() 
 filemenu.add_command(label = "Save                            ", activebackground = "light sky blue", command = save) 
+filemenu.add_command(label = "Save as...                      ", activebackground = "light sky blue") 
 filemenu.add_command(label = "Open...                         ", activebackground = "light sky blue", command = open) 
 filemenu.add_separator()  
-filemenu.add_command(label = "Print                           ", activebackground = "light sky blue")
 filemenu.add_command(label = "Page Format                     ", activebackground = "light sky blue") 
 filemenu.add_separator() 
 filemenu.add_command(label = "Exit                            ", activebackground = "IndianRed1", command = exit)  
@@ -61,11 +105,11 @@ filemenu.add_command(label = "Exit                            ", activebackgroun
 editmenu = Menu(menubar, tearoff = 0) 
 menubar.add_cascade(label = "Edit", menu = editmenu)
 editmenu.add_command(label = "Undo                            ", activebackground = "light sky blue")
-editmenu.add_command(label = "Redo                            ", activebackground = "light sky blue")
+editmenu.add_command(label = "Redo                            ", activebackground = "light sky blue") 
 editmenu.add_separator() 
-editmenu.add_command(label = "Copy                            ", activebackground = "light sky blue")
-editmenu.add_command(label = "Paste                           ", activebackground = "light sky blue")
-editmenu.add_command(label = "Cut                             ", activebackground = "light sky blue") 
+editmenu.add_command(label = "Copy                            ", activebackground = "light sky blue", command = copy)
+editmenu.add_command(label = "Paste                           ", activebackground = "light sky blue", command = paste)
+editmenu.add_command(label = "Cut                             ", activebackground = "light sky blue", command = cut) 
 editmenu.add_command(label = "Delete                          ", activebackground = "light sky blue")
 editmenu.add_separator() 
 editmenu.add_command(label = "Select All                      ", activebackground = "light sky blue")
@@ -81,9 +125,9 @@ viewmenu.add_command(label = "Default Zoom                    ", activebackgroun
 
 helpmenu = Menu(menubar, tearoff = 0)
 menubar.add_cascade(label = "Help", menu = helpmenu)
-helpmenu.add_command(label = "View Help                       ", activebackground = "light sky blue")
+helpmenu.add_command(label = "View Help                       ", activebackground = "light sky blue", command = view_help)
 helpmenu.add_separator() 
-helpmenu.add_command(label = "About Notes                     ", activebackground = "light sky blue")
+helpmenu.add_command(label = "About Notes                     ", activebackground = "light sky blue", command = about_notes)
 
 window.config(menu = menubar)
 window.mainloop()
